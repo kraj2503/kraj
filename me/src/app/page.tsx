6b9f61a6962dc0ca276616c3eacb7ca2c0590834
kraj2503 <srmkr7800@gmail.com>
2024-09-09 18:21:36 +0530

    Appbar and using nextjs

---
+++ me/src/app/page.tsx
@@ -0,0 +1,11 @@
+import AboutMe from "@/components/AboutMe";
+import Appbar from "../components/Appbar";
+
+export default function Home() {
+  return (
+   <>
+<Appbar/>
+<AboutMe/>
+   </>
+  );
+}
---
+++ me/src/components/AboutMe.tsx
@@ -0,0 +1,7 @@
+export default function AboutMe(){
+    return (
+        <div>
+            sd;fjkh;
+        </div>
+    )
+}
